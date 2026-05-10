7f6c44b6e014c12dea60c2f319806453199b1824
Glenn Liu <geliang.liu@ip-paris.fr>
2026-05-10 16:03:34 +0200

    Move the btag eff file in this repository

--- test/example_jetBtag.py
+++ test/example_jetBtag.py
@@ -8,42 +8,24 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import Pos
 from PhysicsTools.NATModules.modules.jetBtag import jetBtag
 
 
-def get_btag_config(era, tag):
-    if era != 2022:
-        raise ValueError("example_jetBtag.py currently has local efficiency JSONs only for 2022/2022EE")
-
-    tagger = "particleNet"
-    tagger_name = "btagPNetB"
-    cmssw_base = os.environ.get("CMSSW_BASE")
-    if cmssw_base is None:
-        cmssw_base = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", ".."))
-    data_dir = os.path.join(cmssw_base, "src", "ZZAnalysis", "NanoAnalysis", "data", "btagEff")
-
-    if "pre_EE" in tag:
-        json_SF = "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2022_Summer22/btagging.json.gz"
-        json_eff = os.path.join(data_dir, "btag_2022.json.gz")
-    else:
-        json_SF = "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2022_Summer22EE/btagging.json.gz"
-        json_eff = os.path.join(data_dir, "btag_2022EE.json.gz")
-
-    return tagger, tagger_name, json_SF, json_eff
+tagger = "particleNet"
+tagger_name = "btagPNetB"
+WP = "M"
+is_mc = True
+json_SF = "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/2022_Summer22EE/btagging.json.gz"
+json_eff = os.path.join(os.path.dirname(__file__), "btag_2022EE.json.gz")
 
 
 parser = ArgumentParser(description="Process nanoAOD files and add b-tagging branches", epilog="Good luck!")
 parser.add_argument("-i", "--infiles", nargs="+")
 parser.add_argument("-m", "--maxevts", type=int, default=1000)
-parser.add_argument("--era", type=int, default=2022)
-parser.add_argument("--tag", default="post_EE", choices=["pre_EE", "post_EE"])
-parser.add_argument("--wp", default="M", choices=["L", "M", "T", "XT", "XXT"])
-parser.add_argument("--data", default=False, action="store_true", help="Run without applying MC SF reshuffling branches")
 args = parser.parse_args()
 
-tagger, tagger_name, json_SF, json_eff = get_btag_config(args.era, args.tag)
 jet_btag = jetBtag(
-    is_mc=not args.data,
+    is_mc=is_mc,
     tagger=tagger,
     tagger_name=tagger_name,
-    WP=args.wp,
+    WP=WP,
     json_SF=json_SF,
     json_eff=json_eff,
 )
